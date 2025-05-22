6aa77953e63ae9eb9087ee7da55c911107eff43b
Dennis Hickox <frozzel@me.com>
2025-05-21 23:19:32 -0400

    added sqlite db

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pennyj.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 app = Flask(__name__)
 
